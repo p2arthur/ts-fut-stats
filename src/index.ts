5d02bd11f40d2be40ff5a22d0b4b48d70edb2463
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-11-23 15:16:12 -0300

    (setup) implement basic analytics to see how many times manchester united won a game using bad code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import * as fs from 'fs';
-console.log('Hello world');
 
-const matches = fs.readFileSync('football.csv', { encoding: 'utf-8' });
+const matches = fs
+  .readFileSync('football.csv', { encoding: 'utf-8' })
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
 
-console.log(matches);
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (
+    (match[1] === 'Man United' && match[5] == 'H') ||
+    (match[2] === 'Man United' && match[5] === 'A')
+  ) {
+    manUnitedWins++;
+  }
+}
+console.log(`Man United won ${manUnitedWins} times`);
