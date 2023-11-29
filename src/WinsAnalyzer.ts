e1e16b6e0a88431938da2c8fe447f4653b491146
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-11-29 20:20:45 -0300

    (feat) implement WinsAnalyzer

---
+++ src/WinsAnalyzer.ts
@@ -0,0 +1,21 @@
+import { MatchData } from './MatchData';
+import { MatchResult } from './MatchResult';
+import { Analyzer } from './Summary';
+
+export class WinsAnalyzer implements Analyzer {
+  constructor(private team: string) {}
+  run(matches: MatchData[]): string {
+    let counter = 0;
+    console.log(this.team);
+    for (let match of matches) {
+      if (
+        (match[1] === this.team && match[5] === MatchResult.homeWin) ||
+        (match[2] === this.team && match[5] === MatchResult.awayWin)
+      ) {
+        counter++;
+      }
+    }
+
+    return `${this.team} won ${counter} times`;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { ConsoleReport } from './ConsoleReport';
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { Summary } from './Summary';
+import { WinsAnalyzer } from './WinsAnalyzer';
 
 //Create an object that satisfies the 'DataReader interface' to be used on the MatchReader - Information source
 const csvFileReader = new CsvFileReader();
@@ -11,8 +12,11 @@ const matchReader = new MatchReader(csvFileReader);
 matchReader.load('football.csv'); //parsed data in MatchResult format
 
 const averageGoalAnalyzer = new AverageGoalsAnalyzer();
+const winsAnalyzer = new WinsAnalyzer('Man City');
 const consoleReport = new ConsoleReport();
 
 const summaryBuilder = new Summary(averageGoalAnalyzer, consoleReport);
+const summaryBuilder2 = new Summary(winsAnalyzer, consoleReport);
 
 summaryBuilder.buildAndPrintReport(matchReader.matches);
+summaryBuilder2.buildAndPrintReport(matchReader.matches);
